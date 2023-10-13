49887d67d6ede54c234cf696b0922a434fe4040e
rodrigopaivadev <rodrigopaiva0502@gmail.com>
2023-10-13 13:41:31 -0300

    :bricks:config eslint/prettier

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
-
 export default function Home() {
   return (
-    <div className="p-8 bg-zinc-50 text-slate-900 dark:bg-zinc-900 dark:text-slate-200 h-screen">
-      <h1 className="font-bold text-3xl sm:text-5xl lg:text-6xl flex items-center gap-3 before:w-0.5 before:h-8 before:bg-emerald-500">Hello tailwind</h1>
-      <button className="bg-emerald-600 text-slate-950 dark:bg-emerald-700 dark:text-slate-50 px-4 py-2 rounded font-medium mt-4 enabled:hover:bg-emerald-600 disabled:opacity-60 disabled:cursor-not-allowed">
+    <div className="h-screen bg-zinc-50 p-8 text-slate-900 dark:bg-zinc-900 dark:text-slate-200">
+      <h1 className="flex items-center gap-3 text-3xl font-bold before:h-8 before:w-0.5 before:bg-emerald-500 sm:text-5xl lg:text-6xl">
+        Hello tailwind
+      </h1>
+      <button className="mt-4 rounded bg-emerald-600 px-4 py-2 font-medium text-slate-950 enabled:hover:bg-emerald-600 disabled:cursor-not-allowed disabled:opacity-60 dark:bg-emerald-700 dark:text-slate-50">
         SignIn
       </button>
     </div>
