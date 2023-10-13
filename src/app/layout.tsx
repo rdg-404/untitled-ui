00ec410e55c11125a0cb55bb1763246534e1e8a5
rodrigopaivadev <rodrigopaiva0502@gmail.com>
2023-10-13 14:07:47 -0300

    :sparkles:config sidebar/grid

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'UI | TailwindCSS'
+  title: 'UI | TailwindCSS',
 }
 
 export default function RootLayout({
@@ -15,7 +15,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className="grid-cols-app grid min-h-screen dark:bg-zinc-950 dark:text-slate-100">
+          <aside className=" border-r border-zinc-200 px-5 py-8 dark:border-zinc-900">
+            sidebar
+          </aside>
+          <main className="px-4 pb-12 pt-8">{children}</main>
+        </div>
+      </body>
     </html>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,3 @@
 export default function Home() {
-  return (
-    <div className="h-screen bg-zinc-50 p-8 text-slate-900 dark:bg-zinc-900 dark:text-slate-200">
-      <h1 className="flex items-center gap-3 text-3xl font-bold before:h-8 before:w-0.5 before:bg-emerald-500 sm:text-5xl lg:text-6xl">
-        Hello tailwind
-      </h1>
-      <button className="mt-4 rounded bg-emerald-600 px-4 py-2 font-medium text-slate-950 enabled:hover:bg-emerald-600 disabled:cursor-not-allowed disabled:opacity-60 dark:bg-emerald-700 dark:text-slate-50">
-        SignIn
-      </button>
-    </div>
-  )
+  return <h1>Hello world</h1>
 }
