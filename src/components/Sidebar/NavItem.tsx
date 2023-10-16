ce4974fd4477acdb0f2c8d7970fe2865e14758f3
rodrigopaivadev <rodrigopaiva0502@gmail.com>
2023-10-16 16:39:20 -0300

    style - fraction value with tailwind

--- src/components/Sidebar/NavItem.tsx
+++ src/components/Sidebar/NavItem.tsx
@@ -14,7 +14,7 @@ export function NavItem({ title, icon: Icon }: NavItemProps) {
     >
       <Icon className="h-5 w-5 text-zinc-500" />
 
-      <span className="font-medium text-slate-100 group-hover:text-slate-300">
+      <span className="font-medium text-slate-100 group-hover:text-slate-400">
         {title}
       </span>
       <ChevronDown className="ml-auto h-5 w-5 text-zinc-400 group-hover:text-zinc-400" />
