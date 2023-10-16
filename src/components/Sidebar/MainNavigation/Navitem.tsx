5111ad2a48fad479cd1409e40dde5a6a922b02ee
rodrigopaivadev <rodrigopaiva0502@gmail.com>
2023-10-16 15:56:56 -0300

    react+ts- user props from components

--- src/components/Sidebar/MainNavigation/Navitem.tsx
+++ src/components/Sidebar/MainNavigation/Navitem.tsx
@@ -1,15 +1,21 @@
-import { ChevronDown, Home } from 'lucide-react'
+import { ChevronDown } from 'lucide-react'
+import { ElementType } from 'react'
 
-export function NavItem() {
+export interface NavItemProps {
+  title: string
+  icon: ElementType
+}
+
+export function NavItem({ title, icon: Icon }: NavItemProps) {
   return (
     <a
       href=""
       className="group flex items-center gap-3 rounded px-3 py-2 hover:bg-violet-950"
     >
-      <Home className="h-5 w-5 text-zinc-500" />
+      <Icon className="h-5 w-5 text-zinc-500" />
 
       <span className="font-medium text-slate-100 group-hover:text-slate-300">
-        Home
+        {title}
       </span>
       <ChevronDown className="ml-auto h-5 w-5 text-zinc-400 group-hover:text-zinc-400" />
     </a>
