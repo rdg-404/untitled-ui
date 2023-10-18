8fec82fe0cb9fd66441e2577c8c65ed51bd57bce
rodrigopaivadev <rodrigopaiva0502@gmail.com>
2023-10-18 15:53:47 -0300

    build- use component pattern created

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -13,20 +13,19 @@ import { Logo } from './Logo'
 import { NavItem } from './NavItem'
 import { UsedSpaceWidget } from './UsedSpaceWidget'
 import { Profile } from './Profile'
+import { Input } from '../Input'
 
 export function Sidebar() {
   return (
     <aside className=" flex flex-col gap-6 border-r border-zinc-200 px-5 py-8 dark:border-zinc-900 dark:text-zinc-300">
       <Logo />
+      <Input.Root>
+        <Input.Prefix>
+          <Search className="h-5 w-5 text-zinc-500" />
+        </Input.Prefix>
 
-      <div className=" mx-1 flex w-full items-center gap-2 rounded-lg border border-zinc-200 px-3 py-2 shadow-sm dark:border-zinc-700">
-        <Search className="h-5 w-5 text-zinc-500 dark:text-zinc-400" />
-        <input
-          className=" flex-1 border-0 bg-transparent p-0 text-zinc-900 placeholder-zinc-600 dark:border-zinc-900"
-          placeholder="Search"
-        />
-      </div>
-
+        <Input.Control placeholder="Search" />
+      </Input.Root>
       <nav className="space-y-0.5">
         <NavItem title="Home" icon={Home} />
         <NavItem title="Dashboard" icon={BarChart} />
