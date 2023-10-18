7bd698968280de5e4a26b3915ba18a3b04ecf08d
rodrigopaivadev <rodrigopaiva0502@gmail.com>
2023-10-18 16:48:31 -0300

    new - aply tabs created radix tabs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
+import { SettingsTabs } from '@/components/SettingsTabs'
+
 export default function Home() {
-  return <h1>Hello world</h1>
+  return (
+    <>
+      <h1 className="to-zinc-900 text-3xl font-medium">Settings</h1>
+      <SettingsTabs />
+    </>
+  )
 }
