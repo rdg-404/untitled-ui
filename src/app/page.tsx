d6ec91c783a9c89821aebe3eb1b24c8c842ac0b6
rodrigopaivadev <rodrigopaiva0502@gmail.com>
2023-10-19 12:43:50 -0300

    style-styled header form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,32 @@ export default function Home() {
     <>
       <h1 className="to-zinc-900 text-3xl font-medium">Settings</h1>
       <SettingsTabs />
+
+      <div className="mt-6 flex flex-col">
+        <div className="flex items-center justify-between  border-b border-zinc-900 pb-5">
+          <div className="space-y-1">
+            <h2 className="text-lg font-medium text-zinc-700">Personal info</h2>
+            <span className="text-sm text-zinc-500">
+              Update your photo and personal details here.
+            </span>
+          </div>
+
+          <div className="flex items-center gap-2">
+            <button
+              type="button"
+              className="rounded-lg border border-zinc-900 px-4 py-2 text-sm font-semibold text-zinc-700 shadow-sm hover:bg-zinc-300"
+            >
+              Cancel
+            </button>
+            <button
+              type="submit"
+              className="rounded-lg border border-zinc-900 px-4 py-2 text-sm font-semibold text-white shadow-sm hover:bg-violet-950"
+            >
+              Save
+            </button>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
