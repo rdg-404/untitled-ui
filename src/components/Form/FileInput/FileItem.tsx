45da8eccfde9fbefb4fadaf97cb6202e77a154d9
rodrigopaivadev <rodrigopaiva0502@gmail.com>
2023-11-13 16:02:16 -0300

    style-responsive sidebar

--- src/components/Form/FileInput/FileItem.tsx
+++ src/components/Form/FileInput/FileItem.tsx
@@ -49,14 +49,14 @@ export function FileItem({ name, size, state }: FileItemProps) {
       {state === 'error' ? (
         <div className="flex flex-1 flex-col items-start gap-1">
           <div className="flex flex-col">
-            <span className="text-error-700 text-sm font-medium">
+            <span className="text-sm font-medium text-error-700">
               Upload Failed! Try again...
             </span>
-            <span className="text-error-500 text-sm ">{name}</span>
+            <span className="text-sm text-error-500 ">{name}</span>
           </div>
           <button
             type="button"
-            className="text-error-700 hover:text-error-900 text-sm font-semibold"
+            className="text-sm font-semibold text-error-700 hover:text-error-900"
           >
             Try again
           </button>
